f0acddb757fbe430b8c7c3452683419db174eea1
tutruong193 <128039200+tutruong193@users.noreply.github.com>
2023-05-17 16:19:23 +0700

    ok

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+//render ra form input
+router.get('/input', function(req, res){
+  res.render('input');
+});
+//lay du lieu tu input
+router.post('/output', function(req, res){
+  var student = req.body;
+  var tuoi = req.query.tuoi;
+  var ten = req.query.ten;
+  var email = req.query.email;
+  var ngaysinh = req.query.ngaysinh;
+  //in ra consolelog
+  // console.log(req.body);
+  // //in ra web voi res.send
+  // res.send(req.body);
+  //render ra trong output kem du lieu tu form
+  res.render('output',{sinhvien : student});
+  //c2res.render('output',{name: ten, email : email, age : tuoi, bod: ngaysinh});
+});
+
+module.exports = router;
